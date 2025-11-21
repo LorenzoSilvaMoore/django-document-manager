b9a65ce2404b5af92421c97a03af6be1e788ac4e
Lorenzo <lorenzosilvamoore@gmail.com>
2025-11-21 16:00:21 +0100

    Update versioning refs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_readme():
 
 setup(
     name="django-document-manager",
-    version="0.2.4",
+    version="0.2.5",
     packages=find_packages(),
     include_package_data=True,
     package_data={
